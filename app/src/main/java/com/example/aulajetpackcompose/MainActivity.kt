4bb1047c13f3c63dcf4ae02a5262b0eab92b9d0e
Talles Guerra <talles-guerra@hotmail.com>
2026-02-07 16:36:49 +0000

    componentes de Interface » FAB, Checkbox, Switch, RadioButton

--- app/src/main/java/com/example/aulajetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/aulajetpackcompose/MainActivity.kt
@@ -1,38 +1,23 @@
 package com.example.aulajetpackcompose
 
-import android.R.attr.top
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
@@ -45,7 +30,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.aulajetpackcompose.model.Usuario
 import com.example.aulajetpackcompose.ui.theme.AulaJetpackComposeTheme
 
@@ -110,6 +94,11 @@ class MainActivity : ComponentActivity() {
         ),
     )
 
+    private val opcoesRadio = listOf(
+        "Opção 1",
+        "Opção 2",
+        "Opção 3",
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -134,6 +123,14 @@ class MainActivity : ComponentActivity() {
             mutableStateOf("")
         }
 
+        var checked by remember {
+            mutableStateOf(false)
+        }
+
+        var radioSelecionado by remember {
+            mutableStateOf(opcoesRadio[0])
+        }
+
         var listaUsuarios by remember {
             mutableStateOf(listOf<Usuario>())
         }
@@ -148,8 +145,7 @@ class MainActivity : ComponentActivity() {
                 .fillMaxHeight()
 
         ) { // Inicio da Nova Interface
-
-            Row(verticalAlignment = Alignment.CenterVertically) {
+               /*         Row(verticalAlignment = Alignment.CenterVertically) {
                 //TextField
                 OutlinedTextField(
                     value = nome,
@@ -196,8 +192,55 @@ class MainActivity : ComponentActivity() {
                          HorizontalDivider()
                 }
             }
+*/
 
-        } // Fechamento Column
+            // Mais componentes de Interface » FAB, Checkbox, Switch, RadioButton
+
+            opcoesRadio.forEach { opcao ->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = opcao == "Opção 1",
+                        onClick = {
+                            radioSelecionado = opcao
+                        })
+                    Text(text = opcao)
+                }
+            }
+
+            Text(text = "Opção Selecionada: $radioSelecionado")
+
+
+
+            /*// Checkbox tem o mesmo comportamento do Switch
+            Switch(
+                checked = checked ,
+                onCheckedChange = { alterado ->
+                    checked = alterado
+                }
+            )
+            Text(text="Alterado:  $checked")
+*/
+
+          /*  //FloatingActionButton
+              ExtendedFloatingActionButton(
+                containerColor = Color.Cyan,
+                contentColor = Color.White,
+
+                onClick = {
+
+            }) {
+             Icon(
+                    painter = painterResource(R.drawable.ic_add_24),
+                    contentDescription = null,
+                )
+
+                Text(text="Pesquisar")
+
+            }*/
+
+      } // Fechamento Column
 
 
     } // Fechamento Segundo App
